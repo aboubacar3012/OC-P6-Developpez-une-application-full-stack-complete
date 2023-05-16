339d7a94f445cadf004675f0f3372af75549b1eb
Aboubacar Diallo <a.diallo3012@gmail.com>
2023-05-16 21:51:03 +0200

    ajout de crud

--- back/src/main/java/com/openclassrooms/mddapi/controller/user/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/user/UserController.java
@@ -1,15 +1,27 @@
 package com.openclassrooms.mddapi.controller.user;
 
+import com.openclassrooms.mddapi.config.JwtTokenUtil;
 import com.openclassrooms.mddapi.exception.NotFoundException;
+import com.openclassrooms.mddapi.model.LoginRegisterResponse;
+import com.openclassrooms.mddapi.model.LoginRequest;
+import com.openclassrooms.mddapi.model.RegistrationRequest;
 import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,13 +29,24 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/auth")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @Autowired
     private UserModelAssembler assembler;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+
 
     @GetMapping()
     public CollectionModel<EntityModel<User>> getUsers(){
@@ -40,10 +63,38 @@ public class UserController {
         return assembler.toModel(user);
     }
 
-    @PostMapping()
-    public ResponseEntity<EntityModel<User>> addUser(@RequestBody User newUser){
-        EntityModel<User> entityModel = assembler.toModel(this.userService.addUser(newUser));
-        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
+//    @PostMapping()
+//    public ResponseEntity<EntityModel<User>> addUser(@RequestBody User newUser){
+//        EntityModel<User> entityModel = assembler.toModel(this.userService.addUser(newUser));
+//        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
+//    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequest request){
+        try{
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+            Authentication authentication = authenticationManager.authenticate(token);
+            User user = (User) authentication.getPrincipal();
+            String jwtToken = jwtTokenUtil.generateAccessToken(user);
+            LoginRegisterResponse response = new LoginRegisterResponse(jwtToken);
+            return ResponseEntity.ok().body(response);
+        }catch (BadCredentialsException exception){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> register(@RequestBody @Valid User user){
+        try{
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            User savedUser = userService.addUser(user);
+            String token = jwtTokenUtil.generateAccessToken(savedUser);
+            LoginRegisterResponse response = new LoginRegisterResponse(token);
+            return ResponseEntity.ok().body(response);
+
+        }catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     @PutMapping("/{id}")
