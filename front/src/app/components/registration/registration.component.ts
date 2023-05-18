8c890d2598e267b01d812fbafdee118c59007e05
Aboubacar Diallo <a.diallo3012@gmail.com>
2023-05-18 20:03:14 +0200

    test

--- front/src/app/components/registration/registration.component.ts
+++ front/src/app/components/registration/registration.component.ts
@@ -20,7 +20,7 @@ export class RegistrationComponent implements OnInit {
   }
 
   onSubmitForm(){
-    console.log(this.registrationForm.value);
+    console.log(this.registrationForm.value)
   }
 
 }
